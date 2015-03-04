1869547be9a1a507188886ac204762d79c90584c
Benoît Dardenne <benoit.dardenne@gmail.com>
2015-03-04 22:50:16 +0100

    First commit

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # fbif
-Facebook Ical Filter
+(Facebook Ical Filter )
+
+Simple script to filter out events from a facebook events ics feed and keep only attending and 'maybe' events.
---
+++ fbif.py
@@ -0,0 +1,39 @@
+#!/usr/bin/python3
+
+import sys
+import uwsgi 
+ 
+import urllib.request
+from urllib.parse import parse_qs, unquote
+from icalendar import Calendar
+
+def application(environ, start_response):
+
+    query = environ['QUERY_STRING']
+    query_t = parse_qs(query)
+    
+    try :
+        url = unquote(query_t['url'][0]) 
+        f = urllib.request.urlopen(url)
+    except (KeyError, ValueError) :
+        start_response('404 NOT FOUND', [('Content-Type', 'text/plain')])
+        return [b"Wrong / No URL specified\n"]
+
+    cal = Calendar.from_ical(f.read());
+
+    output = Calendar()
+
+    # Copy VCALENDAR fields
+    for k,v in cal.items():
+        output.add(k,v)
+
+    # Filter
+    good_events = filter(lambda x: x['partstat'] in ["TENTATIVE", "ACCEPTED"], 
+        cal.walk("VEVENT"))
+
+    for e in good_events:
+        output.add_component(e)
+
+    start_response('200 OK', [('Content-Type', 'text/calendar; charset=utf-8')])
+    return [output.to_ical(True)]
+
